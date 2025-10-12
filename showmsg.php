1da6596b5bbdab4ab573ebfe6ff64f80dfa162bb
Sneha Ghoshal <snehaghoshal577@gmail.com>
2025-10-12 23:02:36 +0530

    Add showmsg.php for displaying current time and greeting based on the hour

---
+++ showmsg.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set("Asia/Kolkata");
+echo "Now The Time is ".date("H:i:sa")."<br>";
+$t=date("H");
+if($t > "5" && $t < "12"){
+echo "Have a Good Morning!";
+}
+elseif ($t >= "12" && $t < "17"){
+echo "Have a Good Ray!"; }
+elseif ($t >= "17" && $t < "21"){
+echo "Have a Good Evening!";
+}else{
+echo "Have a Good Night!";
+}
+?>
